13e4be24bbfc60f1b30914c185c72e7244c59db8
sbhta <68438322+sbhta@users.noreply.github.com>
2021-07-23 02:26:47 +0300

    a

--- MineSweeper_UI-0.2.py
+++ MineSweeper_UI-0.2.py
@@ -1,12 +1,83 @@
 from tkinter import *
-from tkinter.ttk import *
+from random import randint, choice
 
-# 30x16
+
+# 30x16 size in tiles
 root = Tk()
 root.geometry("960x512")
 
+# loading all of the textures
+empty_block = PhotoImage(file="./images/empty-block.png"); empty_block = empty_block.subsample(1, 1)
+unclicked_bomb = PhotoImage(file="./images/unclicked-bomb.png")
+clicked_bomb = PhotoImage(file="./images/bomb-at-clicked-block.png")
+flag = PhotoImage(file="./images/flag.png")
+b0 = PhotoImage(file="./images/0.png")
+b1 = PhotoImage(file="./images/1.png")
+b2 = PhotoImage(file="./images/2.png")
+b3 = PhotoImage(file="./images/3.png")
+b4 = PhotoImage(file="./images/4.png")
+b5 = PhotoImage(file="./images/5.png")
+b6 = PhotoImage(file="./images/6.png")
+b7 = PhotoImage(file="./images/7.png")
+b8 = PhotoImage(file="./images/8.png")
+listOfImages = [b0, b1, b2, b3, b4, b5, b6, b7, b8]
+
+class Tile:
+    def __init__(self, x, y, state=0):
+        self.x = x
+        self.y = y
+        self.state = state
+        self.neighbors = 0
+        self.button = Button(root, image=empty_block, height=26, width=26)
+
+
+    def listNeighbors(self):
+        adj = []
+        try:
+            if self.x - 1 > 0: adj.append(spots[self.y][self.x - 1])
+        except:pass
+
+        try:
+            if self.y - 1 > 0:adj.append(spots[self.y - 1][self.x])
+        except:pass
+
+        try: adj.append(spots[self.y][self.x + 1])
+        except:pass
+        try: adj.append(spots[self.y + 1][self.x])
+        except:pass
+
+        self.neighbors = len(adj)
+            # if spots[self.y][self.x - 1].state == 1: self.neighbors += 1
+            # if spots[self.y][self.x + 1].state == 1: self.neighbors += 1
+            # if spots[self.y + 1][self.x].state == 1: self.neighbors += 1
+            # if spots[self.y - 1][self.x].state == 1: self.neighbors += 1
 
 
 
 
+# global variables
+spots = []
+max_count = 40
+
+
+# creating the board and all of the tiles
+for y in range(16):
+    row = []
+    for x in range(30):
+        row.append(Tile(x, y))
+        row[-1].button.grid(row=y, column=x)
+    spots.append(row)
+# choosing witch tiles are mines
+for i in range(randint(50, 100)):
+    choice(choice(spots)).state = 1
+for y in spots:
+    for x in y:
+        if x.state == 0:
+            x.listNeighbors()
+        if x.state == 1:
+            x.button.config(image=unclicked_bomb)
+for y in spots:
+    for x in y:
+        x.button.config(image=listOfImages[x.neighbors])
+# mainloop of the game
 root.mainloop()
--- test.py
+++ test.py
@@ -1,29 +1,10 @@
 from tkinter import *
-#Create an instance of window or frame
-win= Tk()
-#Set the geometry
-win.geometry("700x600")
-win.resizable(0,0)
-win.config(cursor= "fleur")
-#Let us create a text label
-Label(win, text= "Hover on each of these buttons", font=('Poppins', 20)).pack(pady=20)
 
-#Create some buttons with cursor property
-b1= Button(win, text= "Star",cursor="star")
-b1.pack(pady=20)
-b2= Button(win, text= "Arrow",cursor="arrow")
-b2.pack(pady=20)
-b3= Button(win, text= "Circle",cursor="circle")
-b3.pack(pady=20)
-b4= Button(win, text= "Clock",cursor="clock")
-b4.pack(pady=20)
-b5= Button(win, text= "Heart",cursor="heart")
-b5.pack(pady=20)
-b6= Button(win, text= "Man",cursor="man")
-b6.pack(pady=20)
-b7= Button(win, text= "Mouse",cursor="mouse")
-b7.pack(pady=20)
+root = Tk()
 
-#Keep Running the window
-
-win.mainloop()
+image0 = PhotoImage(file="images/empty-block.png")
+image1 = PhotoImage(file="images/0.png")
+a = [image0, image1]
+b = Button(root, image=a[1])
+b.pack()
+root.mainloop()
